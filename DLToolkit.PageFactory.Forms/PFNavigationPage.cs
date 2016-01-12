4fa83a5af83f74fd626993c03ee3c3ca4ec3487f
Daniel Luberda <daniel.luberda@gmail.com>
2016-01-12 23:24:26 +0100

    Fixed PFNavigationPage typo

--- DLToolkit.PageFactory.Forms/PFNavigationPage.cs
+++ DLToolkit.PageFactory.Forms/PFNavigationPage.cs
@@ -18,6 +18,85 @@ namespace DLToolkit.PageFactory
 		}
 	}
 
+	public abstract class PFNavigationPage<TViewModel> : Xamarin.Forms.NavigationPage, IBasePage<TViewModel> where TViewModel : class, INotifyPropertyChanged
+	{
+		protected PFNavigationPage(bool forcedConstructor = true) : base()
+		{ 
+			PageFactory.ResetPageViewModel(this);
+		}
+
+		protected PFNavigationPage(Xamarin.Forms.Page root, bool forcedConstructor = true) : base(root)
+		{
+			PageFactory.ResetPageViewModel(this);
+		}
+
+		public TViewModel ViewModel
+		{
+			get 
+			{
+				return BindingContext == null ? default(TViewModel) : (TViewModel)BindingContext;
+			}
+		}
+
+		public virtual TViewModel ViewModelInitializer()
+		{
+			return Activator.CreateInstance<TViewModel>();
+		}
+
+		public IPageFactory PageFactory
+		{
+			get
+			{
+				return PF.Factory;
+			}
+		}
+
+		public virtual void PageFactoryMessageReceived(string message, object sender, object arg)
+		{
+		}
+
+		public virtual bool PageFactoryPushing() 
+		{
+			return true;
+		}
+
+		public virtual bool PageFactoryPopping()
+		{
+			return true;
+		}
+
+		public virtual bool PageFactoryInserting() 
+		{
+			return true;
+		}
+
+		public virtual bool PageFactoryRemoving()
+		{
+			return true;
+		}
+
+		public virtual void PageFactoryPushed() 
+		{
+		}
+
+		public virtual void PageFactoryPopped()
+		{
+		}
+
+		public virtual void PageFactoryInserted() 
+		{
+		}
+
+		public virtual void PageFactoryRemoved()
+		{
+		}
+
+		public virtual void PageFactoryRemovingFromCache()
+		{
+		}
+	}
+
+	[Obsolete("Use PFNavigationPage<TViewModel>")]
 	public abstract class NavigationPage<TViewModel> : Xamarin.Forms.NavigationPage, IBasePage<TViewModel> where TViewModel : class, INotifyPropertyChanged
 	{
 		protected NavigationPage(bool forcedConstructor = true) : base()

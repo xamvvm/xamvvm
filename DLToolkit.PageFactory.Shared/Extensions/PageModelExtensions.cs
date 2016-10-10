4775f8c5fe6f8d366c181a04ea645831592d6370
daniel <daniel@daniel.com>
2016-10-10 09:22:00 +0200

    Added some new extensions, refactorings

--- DLToolkit.PageFactory.Shared/Extensions/PageModelExtensions.cs
+++ DLToolkit.PageFactory.Shared/Extensions/PageModelExtensions.cs
@@ -9,54 +9,62 @@ namespace DLToolkit.PageFactory
     public static class PageModelExtensions
     {
         /// <summary>
-        /// Gets the page by page model.
+        /// Gets the current page by page model.
         /// </summary>
         /// <returns>The page.</returns>
         /// <param name="pageModel">Page model.</param>
         /// <typeparam name="TPageModel">Page model type.</typeparam>
-        public static IBasePage<TPageModel> GetPage<TPageModel>(this TPageModel pageModel) where TPageModel : class, IBasePageModel, new()
+        public static IBasePage<TPageModel> GetCurrentPage<TPageModel>(this TPageModel pageModel) where TPageModel : class, IBasePageModel, new()
         {
             return PageFactory.Instance.GetPageByModel(pageModel);
         }
 
-        /// <summary>
-        /// Gets the page page model.
-        /// </summary>
-        /// <returns>The page model.</returns>
-        /// <param name="page">Page.</param>
-        /// <typeparam name="TPageModel">The 1st type parameter.</typeparam>
-        public static TPageModel GetPageModel<TPageModel>(this IBasePage<TPageModel> page) where TPageModel : class, IBasePageModel, new()
-        {
-            return PageFactory.Instance.GetPageModel(page);
-        }
-
 		/// <summary>
-		/// Sets the page page model.
+		/// Gets the page from cache. Creates a new page instances if not exists.
+		/// Optionally provide a page model (else will be set automatically)
 		/// </summary>
-		/// <returns>The page model.</returns>
-		/// <param name="page">Page.</param>
-		/// <param name="newPageModel">New page model.</param>
+		/// <returns>The page from cache.</returns>
+		/// <param name="pageModel">Page model.</param>
+		/// <param name="currentPageModel">Current page model.</param>
+		/// <param name="cacheKey">Cache key.</param>
 		/// <typeparam name="TPageModel">The 1st type parameter.</typeparam>
-		public static IBasePage<TPageModel> SetPageModel<TPageModel>(this IBasePage<TPageModel> page, TPageModel newPageModel) where TPageModel : class, IBasePageModel, new()
+		public static IBasePage<TPageModel> GetPageFromCache<TPageModel>(this IBasePageModel currentPageModel, TPageModel pageModel = null, string cacheKey = null) where TPageModel : class, IBasePageModel, new()
 		{
-			PageFactory.Instance.SetPageModel(page, newPageModel);
-
-			return page;
+			return PageFactory.Instance.GetPageFromCache(pageModel, cacheKey);
 		}
 
 		/// <summary>
-		/// Executes the action on the page model.
+		/// Gets the page as new instance.
+		/// Optionally provide a page model (else will be set automatically)
 		/// </summary>
-		/// <returns>The on page model.</returns>
-		/// <param name="page">Page.</param>
-		/// <param name="action">Action.</param>
+		/// <returns>The page as new instance.</returns>
+		/// <param name="pageModel">Page model.</param>
+		/// <param name="currentPageModel">Current page model.</param>
 		/// <typeparam name="TPageModel">The 1st type parameter.</typeparam>
-		public static IBasePage<TPageModel> ExecuteOnPageModel<TPageModel>(this IBasePage<TPageModel> page, Action<TPageModel> action) where TPageModel : class, IBasePageModel, new()
+		public static IBasePage<TPageModel> GetPageAsNewInstance<TPageModel>(this IBasePageModel currentPageModel, TPageModel pageModel = null) where TPageModel : class, IBasePageModel, new()
 		{
-			var model = page.GetPageModel();
-			action?.Invoke(model);
+			return PageFactory.Instance.GetPageAsNewInstance(pageModel);
+		}
 
-			return page;
+		/// <summary>
+		/// Removes the page type from cache.
+		/// </summary>
+		/// <returns><c>true</c>, if page type from cache was removed, <c>false</c> otherwise.</returns>
+		/// <param name="currentPageModel">Current page model.</param>
+		/// <param name="cacheKey">Cache key.</param>
+		/// <typeparam name="TPageModel">The 1st type parameter.</typeparam>
+		public static bool RemovePageTypeFromCache<TPageModel>(this IBasePageModel currentPageModel, string cacheKey = null) where TPageModel : class, IBasePageModel, new()
+		{
+			return PageFactory.Instance.RemovePageTypeFromCache<TPageModel>(cacheKey);
+		}
+
+		/// <summary>
+		/// Clears the page cache.
+		/// </summary>
+		/// <param name="currentPageModel">Current page model.</param>
+		public static void ClearPageCache(this IBasePageModel currentPageModel)
+		{
+			PageFactory.Instance.ClearPageCache();
 		}
     }
 }

b4eae472dfb93eb3a156832e6e48301010a315c5
escamoteur <github@burkharts.net>
2016-10-23 13:51:14 +0200

    Added Properties to MockFactory so that the result of xamvvm operations can be observed from UnitTests

---
+++ source/Xamvvm.Mock/Factory/ActionResults.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Xamvvm
+{
+	public partial class XamvvmMockFactory : IBaseFactory
+	{
+	    public enum XammvvmAction
+	    {
+            PagePushed, 
+            ModalPagePushed, 
+            PagePopped, 
+            ModalPagePopped, 
+            PoppedToRoot, 
+            SetNewRootAndReset,
+
+            PageInserted, 
+            PageRemoved,
+
+            PageModelChanged,
+
+            PageRemovedFromCache,
+            CacheCleared,
+        };
+
+        // Last Action that was executed
+	    public XammvvmAction LastAction { get; set; }
+
+        // Flag that tells if the LastAction was successfull, e.g. if a Nagigation was intercepted or not.
+        public bool LastActionSuccess { get; set; }
+
+        // PageModel on which the LastAction was executed. If no PageModel was involved it is null 
+	    public IBasePageModel TargetPageModel { get; set; }
+	}
+}
--- source/Xamvvm.Mock/Factory/FactoryNavigation.cs
+++ source/Xamvvm.Mock/Factory/FactoryNavigation.cs
@@ -5,138 +5,210 @@ namespace Xamvvm
 {
 	public partial class XamvvmMockFactory : IBaseFactory
 	{
-		public async virtual Task<bool> PushPageAsync<TCurrentPageModel, TPageModel>(IBasePage<TCurrentPageModel> currentPage, IBasePage<TPageModel> pageToPush, bool animated = true) where TCurrentPageModel : class, IBasePageModel where TPageModel : class, IBasePageModel
+		public  virtual async Task<bool> PushPageAsync<TCurrentPageModel, TPageModel>(IBasePage<TCurrentPageModel> currentPage, IBasePage<TPageModel> pageToPush, bool animated = true) where TCurrentPageModel : class, IBasePageModel where TPageModel : class, IBasePageModel
 		{
-			var navEventsPage = pageToPush as INavigationCanPush;
-			if (navEventsPage != null && !navEventsPage.NavigationCanPush())
-				return false;
+		    TargetPageModel = pageToPush.GetPageModel();
+            LastAction = XammvvmAction.PagePushed;
+
+            var navEventsPage = pageToPush as INavigationCanPush;
+		    if (navEventsPage != null && !navEventsPage.NavigationCanPush())
+		    {
+		        LastActionSuccess = false;
+                return false;
+            }
 
 			var navEventsPageModel = pageToPush.GetPageModel() as INavigationCanPush;
 			if (navEventsPageModel != null && !navEventsPageModel.NavigationCanPush())
-				return false;
+            {
+                LastActionSuccess = false;
+                return false;
+            }
 
-			if (animated)
+            if (animated)
 				await Task.Delay(500);
 
 			var navEventsPage2 = pageToPush as INavigationPushed;
 			if (navEventsPage2 != null)
 				navEventsPage2.NavigationPushed();
 
+		    LastActionSuccess = true;
 			return true;
 		}
 
 		public async virtual Task<bool> PushModalPageAsync<TCurrentPageModel, TPageModel>(IBasePage<TCurrentPageModel> currentPage, IBasePage<TPageModel> pageToPush, bool animated = true) where TCurrentPageModel : class, IBasePageModel where TPageModel : class, IBasePageModel
 		{
-			var navEventsPage = pageToPush as INavigationCanPush;
+            TargetPageModel = pageToPush.GetPageModel();
+            LastAction = XammvvmAction.ModalPagePushed;
+
+            var navEventsPage = pageToPush as INavigationCanPush;
 			if (navEventsPage != null && !navEventsPage.NavigationCanPush())
-				return false;
+            {
+                LastActionSuccess = false;
+                return false;
+            }
 
-			var navEventsPageModel = pageToPush.GetPageModel() as INavigationCanPush;
+            var navEventsPageModel = pageToPush.GetPageModel() as INavigationCanPush;
 			if (navEventsPageModel != null && !navEventsPageModel.NavigationCanPush())
-				return false;
+            {
+                LastActionSuccess = false;
+                return false;
+            }
 
-			if (animated)
+            if (animated)
 				await Task.Delay(500);
 
 			var navEventsPage2 = pageToPush as INavigationPushed;
 			if (navEventsPage2 != null)
 				navEventsPage2.NavigationPushed();
 
-			return true;
+            LastActionSuccess = true;
+            return true;
 		}
 
 		public virtual Task<bool> InsertPageBeforeAsync<TPageModel, TBeforePageModel>(IBasePage<TPageModel> pageToInsert, IBasePage<TBeforePageModel> beforePage) where TPageModel : class, IBasePageModel where TBeforePageModel : class, IBasePageModel
 		{
-			var navEventsPage = pageToInsert as INavigationCanInsert;
-			if (navEventsPage != null && !navEventsPage.NavigationCanInsert())
-				return Task.FromResult(false);
+            TargetPageModel = pageToInsert.GetPageModel();
+            LastAction = XammvvmAction.PageInserted;
 
-			var navEventsPageModel = pageToInsert.GetPageModel() as INavigationCanInsert;
+            var navEventsPage = pageToInsert as INavigationCanInsert;
+		    if (navEventsPage != null && !navEventsPage.NavigationCanInsert())
+		    {
+		        LastActionSuccess = false;
+		        return Task.FromResult(false);
+		    }
+
+		    var navEventsPageModel = pageToInsert.GetPageModel() as INavigationCanInsert;
 			if (navEventsPageModel != null && !navEventsPageModel.NavigationCanInsert())
-				return Task.FromResult(false);
+            {
+                LastActionSuccess = false;
+                return Task.FromResult(false);
+            }
 
-			var navEventsPage2 = pageToInsert as INavigationInserted;
+            var navEventsPage2 = pageToInsert as INavigationInserted;
 			if (navEventsPage2 != null)
 				navEventsPage2.NavigationInserted();
 
+		    LastActionSuccess = true;
 			return Task.FromResult(true);
 		}
 
 		public async virtual Task<bool> PopPageAsync<TCurrentPageModel>(IBasePage<TCurrentPageModel> currentPage, bool animated = true) where TCurrentPageModel : class, IBasePageModel
 		{
-			var navEventsPage = currentPage as INavigationCanPop;
+            TargetPageModel = currentPage.GetPageModel();
+            LastAction = XammvvmAction.PagePopped;
+
+
+            var navEventsPage = currentPage as INavigationCanPop;
 
 			if (navEventsPage != null && !navEventsPage.NavigationCanPop())
-				return false;
+            {
+                LastActionSuccess = false;
+                return false;
+            }
 
-			var navEventsPageModel = currentPage.GetPageModel() as INavigationCanPop;
+            var navEventsPageModel = currentPage.GetPageModel() as INavigationCanPop;
 			if (navEventsPageModel != null && !navEventsPageModel.NavigationCanPop())
-				return false;
+            {
+                LastActionSuccess = false;
+                return false;
+            }
 
-			if (animated)
+            if (animated)
 				await Task.Delay(500);
 
 			var navEventsPage2 = currentPage as INavigationPopped;
 			if (navEventsPage2 != null)
 				navEventsPage2.NavigationPopped();
 
+		    LastActionSuccess = true;
 			return true;
 		}
 
 		public async virtual Task<bool> PopModalPageAsync<TCurrentPageModel>(IBasePage<TCurrentPageModel> currentPage, bool animated = true) where TCurrentPageModel : class, IBasePageModel
 		{
-			var navEventsPage = currentPage as INavigationCanPop;
+            TargetPageModel = currentPage.GetPageModel();
+            LastAction = XammvvmAction.ModalPagePopped;
+
+            var navEventsPage = currentPage as INavigationCanPop;
 
 			if (navEventsPage != null && !navEventsPage.NavigationCanPop())
-				return false;
+            {
+                LastActionSuccess = false;
+                return false;
+            }
 
-			var navEventsPageModel = currentPage.GetPageModel() as INavigationCanPop;
+            var navEventsPageModel = currentPage.GetPageModel() as INavigationCanPop;
 			if (navEventsPageModel != null && !navEventsPageModel.NavigationCanPop())
-				return false;
+            {
+                LastActionSuccess = false;
+                return false;
+            }
 
-			if (animated)
+            if (animated)
 				await Task.Delay(500);
 
 			var navEventsPage2 = currentPage as INavigationPopped;
 			if (navEventsPage2 != null)
-				navEventsPage2.NavigationPopped();
+            {
+                LastActionSuccess = false;
+                return false;
+            }
 
-			return true;
+		    LastActionSuccess = true;
+            return true;
 		}
 
 		public virtual Task<bool> RemovePageAsync<TCurrentPageModel, TPageModel>(IBasePage<TCurrentPageModel> currentPage, IBasePage<TPageModel> pageToRemove) where TCurrentPageModel : class, IBasePageModel where TPageModel : class, IBasePageModel
 		{
-			var navEventsPage = pageToRemove as INavigationCanRemove;
+            TargetPageModel = currentPage.GetPageModel();
+            LastAction = XammvvmAction.PageRemoved;
+
+            var navEventsPage = pageToRemove as INavigationCanRemove;
 			if (navEventsPage != null && !navEventsPage.NavigationCanRemove())
-				return Task.FromResult(false);
+            {
+                LastActionSuccess = false;
+                return Task.FromResult(false);
+            }
 
-			var navEventsPageModel = pageToRemove.GetPageModel() as INavigationCanRemove;
+            var navEventsPageModel = pageToRemove.GetPageModel() as INavigationCanRemove;
 			if (navEventsPageModel != null && !navEventsPageModel.NavigationCanRemove())
-				return Task.FromResult(false);
+            {
+                LastActionSuccess = false;
+                return Task.FromResult(false);
+            }
 
-			var navEventsPage2 = pageToRemove as INavigationRemoved;
+            var navEventsPage2 = pageToRemove as INavigationRemoved;
 			if (navEventsPage2 != null)
 				navEventsPage2.NavigationRemoved();
 
+		    LastActionSuccess = true;
 			return Task.FromResult(true);
 		}
 
 		public async virtual Task<bool> PopPagesToRootAsync<TCurrentPageModel>(IBasePage<TCurrentPageModel> currentPage, bool clearCache = false, bool animated = true) where TCurrentPageModel : class, IBasePageModel
 		{
+		    TargetPageModel = null;
 			if (animated)
 				await Task.Delay(500);
 
 			if (clearCache)
 				ClearPageCache();
 
+            LastAction = XammvvmAction.PoppedToRoot;
+            LastActionSuccess = true;
 			return true;
 		}
 
 		public virtual Task<bool> SetNewRootAndResetAsync<TCurrentPageModel, TPageModel>(IBasePage<TCurrentPageModel> currentPage, IBasePage<TPageModel> newRootPage, bool clearCache = true) where TCurrentPageModel : class, IBasePageModel where TPageModel : class, IBasePageModel
 		{
-			if (clearCache)
+
+            TargetPageModel = newRootPage.GetPageModel();
+
+            if (clearCache)
 				ClearPageCache();
 
+            LastAction = XammvvmAction.SetNewRootAndReset;
+            LastActionSuccess = true;
 			return Task.FromResult(true);
 		}
 	}

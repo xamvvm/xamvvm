e3e38ff88ca998601c779d1f1bd60b0dd988f0a7
Daniel Luberda <danlub01@gmail.com>
2019-02-28 00:14:38 +0100

    WeakEventManager for BaseCommand

--- source/Xamvvm.Core/Implementations/BaseCommand.cs
+++ source/Xamvvm.Core/Implementations/BaseCommand.cs
@@ -6,241 +6,332 @@ using System.Threading.Tasks;
 
 namespace Xamvvm
 {
-	/// <summary>
-	/// xamvvm ICommand implementation.
-	/// </summary>
-	public class BaseCommand<T> : IBaseCommand
-	{
-		readonly Func<T, Task> _execute;
-		readonly Func<T, bool> _canExecuteFunc;
-		bool _canExecute = true;
-
 		/// <summary>
-		/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
+		/// xamvvm ICommand implementation.
 		/// </summary>
-		/// <param name="execute">Execute.</param>
+		public class BaseCommand<T> : IBaseCommand
+		{
+				readonly Func<T, Task> _execute;
+				readonly Func<T, bool> _canExecuteFunc;
+				readonly WeakEventManager _weakEventManager = new WeakEventManager();
+				bool _canExecute = true;
+
+				/// <summary>
+				/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
+				/// </summary>
+				/// <param name="execute">Execute.</param>
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-		public BaseCommand(Action<T> execute) : this(new Func<T, Task>(async (obj) => execute(obj)), null)
+				public BaseCommand(Action<T> execute) : this(new Func<T, Task>(async (obj) => execute(obj)), null)
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
-		{
-			if (execute == null)
-				throw new ArgumentNullException(nameof(execute));
+				{
+						if (execute == null)
+								throw new ArgumentNullException(nameof(execute));
+				}
+
+				/// <summary>
+				/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
+				/// </summary>
+				/// <param name="execute">Execute.</param>
+				public BaseCommand(Func<T, Task> execute) : this(execute, null)
+				{
+				}
+
+				/// <summary>
+				/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
+				/// </summary>
+				/// <param name="execute">Execute.</param>
+				/// <param name="canExecute">Can execute.</param>
+				public BaseCommand(Func<T, Task> execute, Func<T, bool> canExecute)
+				{
+						if (execute == null)
+								throw new ArgumentNullException(nameof(execute));
+
+						_execute = execute;
+						_canExecuteFunc = canExecute;
+				}
+
+				/// <summary>
+				/// Occurs when can execute changed.
+				/// </summary>
+				public event EventHandler CanExecuteChanged
+				{
+						add { _weakEventManager.AddEventHandler(value); }
+						remove { _weakEventManager.RemoveEventHandler(value); }
+				}
+
+				/// <summary>
+				/// Raises the can execute changed.
+				/// </summary>
+				public void RaiseCanExecuteChanged()
+				{
+						_weakEventManager.HandleEvent(this, EventArgs.Empty, nameof(CanExecuteChanged));
+				}
+
+				/// <summary>
+				/// Determines whether this instance can execute the specified parameter.
+				/// </summary>
+				/// <returns><c>true</c> if this instance can execute the specified parameter; otherwise, <c>false</c>.</returns>
+				/// <param name="parameter">Parameter.</param>
+				[DebuggerStepThrough]
+				public bool CanExecute(object parameter)
+				{
+						return _canExecute && (_canExecuteFunc == null || _canExecuteFunc((T)parameter));
+				}
+
+				/// <summary>
+				/// Execute the specified action.
+				/// </summary>
+				/// <param name="parameter">Parameter.</param>
+				public async void Execute(object parameter)
+				{
+						try
+						{
+								_canExecute = false;
+								RaiseCanExecuteChanged();
+								if (parameter == null)
+										await _execute(default(T));
+								else
+										await _execute((T)parameter);
+						}
+						finally
+						{
+								_canExecute = true;
+								RaiseCanExecuteChanged();
+						}
+				}
 		}
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
+		/// xamvvm ICommand implementation.
 		/// </summary>
-		/// <param name="execute">Execute.</param>
-		public BaseCommand(Func<T, Task> execute) : this(execute, null)
+		public class BaseCommand : IBaseCommand
 		{
-		}
+				readonly Func<object, Task> _execute;
+				readonly Func<bool> _canExecuteFunc;
+				readonly WeakEventManager _weakEventManager = new WeakEventManager();
+				bool _canExecute = true;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
-		/// </summary>
-		/// <param name="execute">Execute.</param>
-		/// <param name="canExecute">Can execute.</param>
-		public BaseCommand(Func<T, Task> execute, Func<T, bool> canExecute)
-		{
-			if (execute == null)
-				throw new ArgumentNullException(nameof(execute));
-
-			_execute = execute;
-			_canExecuteFunc = canExecute;
-		}
-
-		/// <summary>
-		/// Occurs when can execute changed.
-		/// </summary>
-		public event EventHandler CanExecuteChanged;
-
-		/// <summary>
-		/// Raises the can execute changed.
-		/// </summary>
-		public void RaiseCanExecuteChanged()
-		{
-			var handler = CanExecuteChanged;
-			if (handler != null)
-			{
-				handler(this, EventArgs.Empty);
-			}
-		}
-
-		/// <summary>
-		/// Determines whether this instance can execute the specified parameter.
-		/// </summary>
-		/// <returns><c>true</c> if this instance can execute the specified parameter; otherwise, <c>false</c>.</returns>
-		/// <param name="parameter">Parameter.</param>
-		[DebuggerStepThrough]
-		public bool CanExecute(object parameter)
-		{
-			return _canExecute && (_canExecuteFunc == null || _canExecuteFunc((T)parameter));
-		}
-
-		/// <summary>
-		/// Execute the specified action.
-		/// </summary>
-		/// <param name="parameter">Parameter.</param>
-		public async void Execute(object parameter)
-		{
-			try
-			{
-				_canExecute = false;
-				RaiseCanExecuteChanged();
-				if (parameter == null)
-					await _execute(default(T));
-				else
-					await _execute((T)parameter);
-			}
-			finally
-			{
-				_canExecute = true;
-				RaiseCanExecuteChanged();
-			}
-		}		
-	}
-
-	/// <summary>
-	/// xamvvm ICommand implementation.
-	/// </summary>
-	public class BaseCommand : IBaseCommand
-	{
-		/// <summary>
-		/// Creates command from the task.
-		/// </summary>
-		/// <returns>The task.</returns>
-		/// <param name="taskFunc">Task func.</param>
-		/// <param name="canExecute">Can execute.</param>
-		/// <typeparam name="TParam">The 1st type parameter.</typeparam>
-		public static BaseCommand<TParam> FromTask<TParam>(Func<TParam, Task> taskFunc, Func<TParam, bool> canExecute = null)
-		{
-			return new BaseCommand<TParam>((arg) => taskFunc?.Invoke(arg), canExecute);
-		}
-
-		/// <summary>
-		/// Creates command from the task.
-		/// </summary>
-		/// <returns>The task.</returns>
-		/// <param name="taskFunc">Task func.</param>
-		/// <param name="canExecute">Can execute.</param>
-		public static BaseCommand FromTask(Func<object, Task> taskFunc, Func<bool> canExecute = null)
-		{
-			return new BaseCommand((arg) => taskFunc?.Invoke(arg), canExecute);
-		}
-
-		/// <summary>
-		/// Creates command from the action.
-		/// </summary>
-		/// <returns>The action.</returns>
-		/// <param name="taskFunc">Task func.</param>
-		/// <param name="canExecute">Can execute.</param>
-		/// <typeparam name="TParam">The 1st type parameter.</typeparam>
-		public static BaseCommand<TParam> FromAction<TParam>(Action<TParam> taskFunc, Func<TParam, bool> canExecute = null)
-		{
+				/// <summary>
+				/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
+				/// </summary>
+				/// <param name="execute">Execute.</param>
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-			return new BaseCommand<TParam>(async (arg) => taskFunc?.Invoke(arg), canExecute);
+				public BaseCommand(Action<object> execute) : this(new Func<object, Task>(async (obj) => execute(obj)), null)
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+				{
+						if (execute == null)
+								throw new ArgumentNullException(nameof(execute));
+				}
+
+				/// <summary>
+				/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
+				/// </summary>
+				/// <param name="execute">Execute.</param>
+				public BaseCommand(Func<object, Task> execute) : this(execute, null)
+				{
+				}
+
+				/// <summary>
+				/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
+				/// </summary>
+				/// <param name="execute">Execute.</param>
+				/// <param name="canExecute">Can execute.</param>
+				public BaseCommand(Func<object, Task> execute, Func<bool> canExecute)
+				{
+						if (execute == null)
+						{
+								throw new ArgumentNullException(nameof(execute));
+						}
+
+						_execute = execute;
+						_canExecuteFunc = canExecute;
+				}
+
+				/// <summary>
+				/// Occurs when can execute changed.
+				/// </summary>
+				public event EventHandler CanExecuteChanged
+				{
+						add { _weakEventManager.AddEventHandler(value); }
+						remove { _weakEventManager.RemoveEventHandler(value); }
+				}
+
+				/// <summary>
+				/// Raises the can execute changed.
+				/// </summary>
+				public void RaiseCanExecuteChanged()
+				{
+						_weakEventManager.HandleEvent(this, EventArgs.Empty, nameof(CanExecuteChanged));
+				}
+
+				/// <summary>
+				/// Determines whether this instance can execute the specified parameter.
+				/// </summary>
+				/// <returns><c>true</c> if this instance can execute the specified parameter; otherwise, <c>false</c>.</returns>
+				/// <param name="parameter">Parameter.</param>
+				[DebuggerStepThrough]
+				public bool CanExecute(object parameter)
+				{
+						return _canExecute && (_canExecuteFunc == null || _canExecuteFunc());
+				}
+
+				/// <summary>
+				/// Execute the specified action.
+				/// </summary>
+				/// <param name="parameter">Parameter.</param>
+				public async void Execute(object parameter)
+				{
+						try
+						{
+								_canExecute = false;
+								RaiseCanExecuteChanged();
+								await _execute(parameter);
+						}
+						finally
+						{
+								_canExecute = true;
+								RaiseCanExecuteChanged();
+						}
+				}
 		}
 
-		/// <summary>
-		/// Creates command from the action.
-		/// </summary>
-		/// <returns>The action.</returns>
-		/// <param name="action">Action.</param>
-		/// <param name="canExecute">Can execute.</param>
-		public static BaseCommand FromAction(Action<object> action, Func<bool> canExecute = null)
+		class WeakEventManager
 		{
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-			return new BaseCommand(async(arg) => action(arg), canExecute);
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+				readonly Dictionary<string, List<Subscription>> _eventHandlers = new Dictionary<string, List<Subscription>>();
+
+				public void AddEventHandler<TEventArgs>(EventHandler<TEventArgs> handler, [CallerMemberName]string eventName = null)
+						where TEventArgs : EventArgs
+				{
+						if (string.IsNullOrEmpty(eventName))
+								throw new ArgumentNullException(nameof(eventName));
+
+						if (handler == null)
+								throw new ArgumentNullException(nameof(handler));
+
+						AddEventHandler(eventName, handler.Target, handler.GetMethodInfo());
+				}
+
+				public void AddEventHandler(EventHandler handler, [CallerMemberName]string eventName = null)
+				{
+						if (string.IsNullOrEmpty(eventName))
+								throw new ArgumentNullException(nameof(eventName));
+
+						if (handler == null)
+								throw new ArgumentNullException(nameof(handler));
+
+						AddEventHandler(eventName, handler.Target, handler.GetMethodInfo());
+				}
+
+				public void HandleEvent(object sender, object args, string eventName)
+				{
+						var toRaise = new List<(object subscriber, MethodInfo handler)>();
+						var toRemove = new List<Subscription>();
+
+						if (_eventHandlers.TryGetValue(eventName, out List<Subscription> target))
+						{
+								for (int i = 0; i < target.Count; i++)
+								{
+										Subscription subscription = target[i];
+										bool isStatic = subscription.Subscriber == null;
+										if (isStatic)
+										{
+												// For a static method, we'll just pass null as the first parameter of MethodInfo.Invoke
+												toRaise.Add((null, subscription.Handler));
+												continue;
+										}
+
+										object subscriber = subscription.Subscriber.Target;
+
+										if (subscriber == null)
+												// The subscriber was collected, so there's no need to keep this subscription around
+												toRemove.Add(subscription);
+										else
+												toRaise.Add((subscriber, subscription.Handler));
+								}
+
+								for (int i = 0; i < toRemove.Count; i++)
+								{
+										Subscription subscription = toRemove[i];
+										target.Remove(subscription);
+								}
+						}
+
+						for (int i = 0; i < toRaise.Count; i++)
+						{
+								(var subscriber, var handler) = toRaise[i];
+								handler.Invoke(subscriber, new[] { sender, args });
+						}
+				}
+
+				public void RemoveEventHandler<TEventArgs>(EventHandler<TEventArgs> handler, [CallerMemberName]string eventName = null)
+						where TEventArgs : EventArgs
+				{
+						if (string.IsNullOrEmpty(eventName))
+								throw new ArgumentNullException(nameof(eventName));
+
+						if (handler == null)
+								throw new ArgumentNullException(nameof(handler));
+
+						RemoveEventHandler(eventName, handler.Target, handler.GetMethodInfo());
+				}
+
+				public void RemoveEventHandler(EventHandler handler, [CallerMemberName]string eventName = null)
+				{
+						if (string.IsNullOrEmpty(eventName))
+								throw new ArgumentNullException(nameof(eventName));
+
+						if (handler == null)
+								throw new ArgumentNullException(nameof(handler));
+
+						RemoveEventHandler(eventName, handler.Target, handler.GetMethodInfo());
+				}
+
+				void AddEventHandler(string eventName, object handlerTarget, MethodInfo methodInfo)
+				{
+						if (!_eventHandlers.TryGetValue(eventName, out List<Subscription> targets))
+						{
+								targets = new List<Subscription>();
+								_eventHandlers.Add(eventName, targets);
+						}
+
+						if (handlerTarget == null)
+						{
+								// This event handler is a static method
+								targets.Add(new Subscription(null, methodInfo));
+								return;
+						}
+
+						targets.Add(new Subscription(new WeakReference(handlerTarget), methodInfo));
+				}
+
+				void RemoveEventHandler(string eventName, object handlerTarget, MemberInfo methodInfo)
+				{
+						if (!_eventHandlers.TryGetValue(eventName, out List<Subscription> subscriptions))
+								return;
+
+						for (int n = subscriptions.Count; n > 0; n--)
+						{
+								Subscription current = subscriptions[n - 1];
+
+								if (current.Subscriber?.Target != handlerTarget || current.Handler.Name != methodInfo.Name)
+										continue;
+
+								subscriptions.Remove(current);
+								break;
+						}
+				}
+
+				struct Subscription
+				{
+						public Subscription(WeakReference subscriber, MethodInfo handler)
+						{
+								Subscriber = subscriber;
+								Handler = handler ?? throw new ArgumentNullException(nameof(handler));
+						}
+
+						public readonly WeakReference Subscriber;
+						public readonly MethodInfo Handler;
+				}
 		}
-
-		readonly Func<object, Task> _execute;
-		readonly Func<bool> _canExecuteFunc;
-		bool _canExecute = true;
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
-		/// </summary>
-		/// <param name="execute">Execute.</param>
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-		public BaseCommand(Action<object> execute) : this(new Func<object, Task>(async (obj) => execute(obj)), null)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
-		{
-			if (execute == null)
-				throw new ArgumentNullException(nameof(execute));
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
-		/// </summary>
-		/// <param name="execute">Execute.</param>
-		public BaseCommand(Func<object, Task> execute) : this(execute, null)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Xamvvm.BaseCommand"/> class.
-		/// </summary>
-		/// <param name="execute">Execute.</param>
-		/// <param name="canExecute">Can execute.</param>
-		public BaseCommand(Func<object, Task> execute, Func<bool> canExecute)
-		{
-			if (execute == null)
-			{
-				throw new ArgumentNullException(nameof(execute));
-			}
-
-			_execute = execute;
-			_canExecuteFunc = canExecute;
-		}
-
-		/// <summary>
-		/// Occurs when can execute changed.
-		/// </summary>
-		public event EventHandler CanExecuteChanged;
-
-		/// <summary>
-		/// Raises the can execute changed.
-		/// </summary>
-		public void RaiseCanExecuteChanged()
-		{
-			var handler = CanExecuteChanged;
-			if (handler != null)
-			{
-				handler(this, EventArgs.Empty);
-			}
-		}
-
-		/// <summary>
-		/// Determines whether this instance can execute the specified parameter.
-		/// </summary>
-		/// <returns><c>true</c> if this instance can execute the specified parameter; otherwise, <c>false</c>.</returns>
-		/// <param name="parameter">Parameter.</param>
-		[DebuggerStepThrough]
-		public bool CanExecute(object parameter)
-		{
-			return _canExecute && (_canExecuteFunc == null || _canExecuteFunc());
-		}
-			
-		/// <summary>
-		/// Execute the specified action.
-		/// </summary>
-		/// <param name="parameter">Parameter.</param>
-		public async void Execute(object parameter)
-		{
-			try
-			{
-				_canExecute = false;
-				RaiseCanExecuteChanged();
-				await _execute(parameter);
-			}
-			finally
-			{
-				_canExecute = true;
-				RaiseCanExecuteChanged();
-			}
-		}
-	}
 }
 
